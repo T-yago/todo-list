7edbdba34ea45372a5f6510a97f42f5f5b1c0f57
a100665 <101202571+T-yago@users.noreply.github.com>
2023-07-23 23:01:29 +0100

    There is no more a view folder

--- lib/todo_list_web/views/task_view.ex
+++
@@ -1,7 +0,0 @@
-defmodule TodoListWeb.TaskView do
-  use TodoListWeb, :view
-
-  def render("task.json", %{task: task}) do
-    %{id: task.id, name: task.name, description: task.description, status: task.status, timestamp: task.timestamp, inserted_at: task.inserted_at, updated_at: task.updated_at}
-  end
-end
